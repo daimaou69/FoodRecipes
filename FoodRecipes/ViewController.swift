7cde33ec016d677fe935084e3477d4525b08ce2b
Tran Thanh Tung <thanhtungt447@gmail.com>
2021-04-17 09:17:57 +0700

    Initial Commit

---
+++ FoodRecipes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodRecipes
+//
+//  Created by CNTT on 4/17/21.
+//  Copyright © 2021 fit.tdc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
