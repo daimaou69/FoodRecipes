c897a896cd47adca292479434b43caf1ee46d4b6
Tran Thanh Tung <thanhtungt447@gmail.com>
2021-04-17 11:29:03 +0700

    Update firebase test function

--- FoodRecipes/ViewController.swift
+++ FoodRecipes/ViewController.swift
@@ -7,14 +7,32 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var txtUserName: UITextField!
+    
+    @IBOutlet weak var txtPassword: UITextField!
+    
+    var ref: DatabaseReference!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func btnRegistry(_ sender: UIButton) {
+        ref = Database.database().reference()
+        
+        let id = ref.childByAutoId()
+        
+        self.ref.child("User").child(id.key!).setValue([
+            "userID":id.key!,
+            "userName": txtUserName.text!,
+            "password": txtPassword.text!])
+    }
 }
 
